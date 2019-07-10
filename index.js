4d1f24e48427c53642d8fd2af64449c6a33ec2c1
Joe Wesemann <jwesemann@plaid.com>
2019-07-10 11:32:22 -0700

    keep track of incidents and their status in memory

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
 
+const pagerduty_incidents = [];
+
 const APP_PORT = 8080;
 
 const app = express();
@@ -12,6 +14,17 @@ app.get('/', (req, res) => {
 
 app.post('/pagerduty-alert', (req, res) => {
   console.log(JSON.stringify(req.body, null, 4));
+
+  const incident = req.body.messages[0];
+  if (incident.event === 'incident.trigger') {
+    pagerduty_incidents.push({
+      id: incident.incident.incident_number,
+      title: incident.incident.title,
+      status: incident.event,
+    });
+  }
+
+  console.log(pagerduty_incidents);
   res.json({error: null});
 });
 
