a61541d0b43e87f12dd2d2928acf6a35ae454285
Joe Wesemann <jwesemann@plaid.com>
2019-07-10 11:07:57 -0700

    pager duty alert endpoint

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import * as bodyParser from 'body-parser';
 
 const APP_PORT = 8080;
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.json({error: null, msg: 'Hello World!'});
+});
+
+app.post('/pagerduty-alert', (req, res) => {
+  console.log(JSON.stringify(req.body, null, 4));
+  res.json({error: null});
 });
 
 app.listen(APP_PORT, () => {
